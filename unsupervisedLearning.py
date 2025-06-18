74bb4b18574dbd768c6a87ff4a1866be545a4ab2
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2025-06-18 21:40:58 +0300

    added

---
+++ unsupervisedLearning.py
@@ -0,0 +1,28 @@
+#unsupervised learning
+#what is unsupervised learning? unlabbelled' unstructured data, pattern recognition, clustering, dimensionality reduction
+# characteristics of unsupervised learning:
+# - no labelled data is provided
+# - can handle large datasets
+# - used for EDA (exploratory data analysis)
+# - can be used for feature extraction
+
+#techniques:
+# - clustering: grouping similar data points together
+# - dimensionality reduction: reducing the number of features in a dataset while preserving its structure
+# - association rule learning: finding interesting relationships between variables in large datasets
+# - anomaly detection: identifying rare items, events or observations that raise suspicions by differing significantly from the majority of the data
+
+#clustering
+# - k-means clustering: partitions data into k clusters based on similarity
+# - hierarchical clustering: builds a hierarchy of clusters by either merging or splitting them
+# - DBSCAN: density-based spatial clustering of applications with noise, groups together points that are closely packed together while marking points in low-density regions as outliers
+
+#advantages of unsupervised learning:
+# - can discover hidden patterns in data
+# - can handle large datasets
+# - no need for labelled data
+# - its flexible and can be used for various tasks
+
+#example one: real problem
+#create a dataset to cluster customers based on their annual income and spending
+#K-means clustering
